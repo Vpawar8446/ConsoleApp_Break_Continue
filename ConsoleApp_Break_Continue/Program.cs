3652cbb2e34efa1cc2b3f5818e4fb652a6ac8de3
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 23:39:58 +0530

    Add project files.

---
+++ ConsoleApp_Break_Continue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp_Break_Continue
+{
+    class Conbre
+    {
+        public void Display(int n)
+        {
+            int i;
+            for(i=0;i<=n;i++)
+            {
+                if (i > 1 && i<5)
+                     continue;
+
+                else if (i == 8)
+                       break;
+                
+                Console.WriteLine("Value is " + i);
+            }
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conbre obj = new Conbre();
+            Console.Write("Enter the any number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            obj.Display(num);
+        }
+    }
+}
